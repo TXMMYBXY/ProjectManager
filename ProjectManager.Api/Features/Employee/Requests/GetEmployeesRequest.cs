c49252c4e4a7a43afc5b1afbaa134502c18dd892
TXMMYBXY <altusha_03@bk.ru>
2026-07-05 18:51:56 +0700

    remove properties from queryParams

--- ProjectManager.Api/Features/Employee/Requests/GetEmployeesRequest.cs
+++ ProjectManager.Api/Features/Employee/Requests/GetEmployeesRequest.cs
@@ -1,26 +1,17 @@
-using System.Text.Json.Serialization;
 using ProjectManager.Application.Employee;
 
 namespace ProjectManager.Api.Features.Employee.Requests;
 
 public class GetEmployeesRequest
 {
-    [JsonPropertyName("sortField")]
     public EmployeeSortField? SortField { get; set; }
-    [JsonPropertyName("descending")]
     public bool Descending { get; set; }
     
-    [JsonPropertyName("firstName")]
     public string? FirstName { get; set; }
-    [JsonPropertyName("lastName")]
     public string? LastName { get; set; }
-    [JsonPropertyName("patronymic")]
     public string? Patronymic { get; set; }
-    [JsonPropertyName("email")]
     public string? Email { get; set; }
     
-    [JsonPropertyName("pageSize")]
     public int? PageSize { get; set; }
-    [JsonPropertyName("pageNumber")]
     public int? PageNumber { get; set; }
 }
--- ProjectManager.Api/Features/Issue/Requests/GetIssuesRequest.cs
+++ ProjectManager.Api/Features/Issue/Requests/GetIssuesRequest.cs
@@ -1,4 +1,3 @@
-using System.Text.Json.Serialization;
 using ProjectManager.Application.Issue;
 using ProjectManager.Entities.Enums;
 
@@ -6,26 +5,16 @@ namespace ProjectManager.Api.Features.Issue.Requests;
 
 public class GetIssuesRequest
 {
-    [JsonPropertyName("sortField")]
     public IssueSortField? SortField { get; set; }
-    [JsonPropertyName("descending")]
     public bool Descending { get; set; }
     
-    [JsonPropertyName("title")]
     public string? Title { get; set; }
-    [JsonPropertyName("status")]
     public IssueStatus? Status { get; set; }
-    [JsonPropertyName("priority")]
     public byte? Priority { get; set; }
-    [JsonPropertyName("projectTitle")]
     public string? ProjectTitle { get; set; }
-    [JsonPropertyName("authorFullName")]
     public string? AuthorFullName { get; set; }
-    [JsonPropertyName("executorFullName")]
     public string? ExecutorFullName { get; set; }
     
-    [JsonPropertyName("pageSize")]
     public int? PageSize { get; set; }
-    [JsonPropertyName("pageNumber")]
     public int? PageNumber { get; set; }
 }
